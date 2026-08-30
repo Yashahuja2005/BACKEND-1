e8d8552bf79ddad2119225e814111a8141567f29
Yashahuja2005 <ahujayash756@gmail.com>
2026-08-30 23:00:58 +0530

    DAY-5 | BACKEND DONE

--- DAY-9/BACKEND/src/app.js
+++ DAY-9/BACKEND/src/app.js
@@ -2,9 +2,12 @@ const express = require("express")
 
 const noteModel = require("./models/note.model")
 
+const cors = require("cors")
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.post("/api/notes", async(req,res)=>{
     const { title, description } = req.body
